c05143530723bfb7fdb89887791d6b4da9f0022e
alexmpalermo <alexmpalermo@gmail.com>
2019-05-21 15:05:36 -0400

    Edit routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/home' => 'users#home', :as => :home
+  get '/signup' => 'users#signup', :as => :signup
+  post '/signup' => 'users#create', :as => :signup
+  get '/login' => 'sessions#login', :as => :login
+  post '/login' => 'sessions#create', :as => :login
+  get '/logout' => 'sessions#destroy', :as => :logout
+
+  resources :users
+
+  resources :dispensers do
+    resources :plants
+  end
+
+
 end
