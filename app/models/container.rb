c48251171b71b61dcbf9da735cc4fedb16a20c0e
alexmpalermo <alexmpalermo@gmail.com>
2019-05-22 08:43:46 -0400

    Create new methods in watering and container models

--- app/models/container.rb
+++ app/models/container.rb
@@ -2,4 +2,9 @@ class Container < ApplicationRecord
   belongs_to :dispenser
   has_many :waterings
   has_many :plants, :through => :waterings
+
+  def self.refill(disp)
+    refill = self.create(:dispenser_id => disp.id, :start_amount => disp.capacity, :date => Date.current)
+    refill
+  end 
 end
--- app/models/watering.rb
+++ app/models/watering.rb
@@ -1,4 +1,17 @@
 class Watering < ApplicationRecord
   belongs_to :plant
   belongs_to :container
+  validates :vacation_days, numericality: { greater_than: 0 }
+
+  def starting_amount(lr, lc)
+    if lr.leftover == 0 && lc.start_amount == 0
+      @new_amount = lc.dispenser.capacity
+      lc.update(:start_amount => @new_amount, :date => Date.current)
+      @new_amount
+    else
+      lr.leftover
+    end
+  end
+
+
 end
