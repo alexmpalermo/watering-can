97000af8f4ce92370d893040a77ba538205cca4a
alexmpalermo <alexmpalermo@gmail.com>
2019-05-23 14:40:37 -0400

    Add water and water_my_plants method, add partial file

--- app/models/watering.rb
+++ app/models/watering.rb
@@ -13,5 +13,31 @@ class Watering < ApplicationRecord
     end
   end
 
+  def self.water(disp_id)
+    @dispenser = Dispenser.find_by_id(disp_id)
+    @container = @dispenser.containers.last
+    @last = @container.waterings.last
+    @plants = Plant.water_today.where(:dispenser_id => @dispenser.id)
+    @plants.each do |plant|
+      @watering = Watering.new(:plant_id => plant.id, :date => Date.current, :container_id => @container.id, :end_vacation => @last.end_vacation, :start_vacation => @last.start_vacation, :leftover => 0)
+      @watering.vacation_days = (@watering.end_vacation - Date.current)
+      if @watering.save
+        @watering.update(:leftover => (@watering.starting_amount(@last, @container) - plant.water_quantity))
+        @next = (@watering.date + plant.water_frequency)
+        plant.update(:last_day_watered => @watering.date, :next_water_day => @next)
+        if @watering.leftover < 0
+          @disp = Dispenser.find_by_id(@watering.container.dispenser_id)
+          @c_new = Container.refill(@disp)
+          @new_leftover = (@c_new.start_amount + @watering.leftover)
+          Watering.create(:plant_id => plant.id, :container_id => @c_new.id, :vacation_days => @watering.vacation_days, :start_vacation => @watering.start_vacation, :end_vacation => @watering.end_vacation, :date => @watering.date, :leftover => @new_leftover)
+        end
+        @last_plant_water = plant.waterings.last
+        plant.check_water(@last_plant_water.vacation_days)
+      end
+    end
+  end
 
+  def self.water_my_plants(disp_id)
+    self.water(disp_id) until !Plant.water_soon.where(:dispenser_id => disp_id)
+  end
 end
