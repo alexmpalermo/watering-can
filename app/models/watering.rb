bbee6ff316c85192e4fd165d6e294614ca6b4bb9
alexmpalermo <alexmpalermo@gmail.com>
2019-05-23 17:06:32 -0400

    Add to water method

--- app/models/watering.rb
+++ app/models/watering.rb
@@ -1,7 +1,7 @@
 class Watering < ApplicationRecord
   belongs_to :plant
   belongs_to :container
-  validates :vacation_days, numericality: { greater_than: 0 }
+  validates :vacation_days, numericality: { greater_than_or_equal_to: 0 }
 
   def starting_amount(lr, lc)
     if lr.leftover == 0 && lc.start_amount == 0
@@ -14,15 +14,12 @@ class Watering < ApplicationRecord
   end
 
   def self.water(disp_id)
-
     if @plants = Plant.water_today.where(:dispenser_id => disp_id)
-    @dispenser = Dispenser.find_by_id(disp_id)
-    @container = @dispenser.containers.last
-    @last = @container.waterings.last
-    @plants.each do |plant|
-      @watering = Watering.new(:plant_id => plant.id, :date => Date.current, :container_id => @container.id, :end_vacation => @last.end_vacation, :start_vacation => @last.start_vacation, :leftover => 0)
-      @watering.vacation_days = (@watering.end_vacation - Date.current)
-      if @watering.save
+      @dispenser = Dispenser.find_by_id(disp_id)
+      @container = @dispenser.containers.last
+      @last = @container.waterings.last
+      @plants.each do |plant|
+        @watering = Watering.create(:plant_id => plant.id, :date => Date.current, :container_id => @container.id, :end_vacation => @last.end_vacation, :start_vacation => @last.start_vacation, :leftover => 0, :vacation_days => (@last.end_vacation - Date.current))
         @watering.update(:leftover => (@watering.starting_amount(@last, @container) - plant.water_quantity))
         @next = (@watering.date + plant.water_frequency)
         plant.update(:last_day_watered => @watering.date, :next_water_day => @next)
@@ -30,17 +27,13 @@ class Watering < ApplicationRecord
           @disp = Dispenser.find_by_id(@watering.container.dispenser_id)
           @c_new = Container.refill(@disp)
           @new_leftover = (@c_new.start_amount + @watering.leftover)
-          Watering.create(:plant_id => plant.id, :container_id => @c_new.id, :vacation_days => @watering.vacation_days, :start_vacation => @watering.start_vacation, :end_vacation => @watering.end_vacation, :date => @watering.date, :leftover => @new_leftover)
-
+          @w_new = Watering.create(:plant_id => plant.id, :container_id => @c_new.id, :vacation_days => @watering.vacation_days, :start_vacation => @watering.start_vacation, :end_vacation => @watering.end_vacation, :date => @watering.date, :leftover => @new_leftover)
+          plant.check_water(@w_new.vacation_days)
+        else
+          plant.check_water(@watering.vacation_days)
         end
-        @last_plant_water = plant.waterings.last
-        plant.check_water(@last_plant_water.vacation_days)
-      end
+      end 
     end
-
-
-
-
   end
 
   def self.water_my_plants(disp_id)
