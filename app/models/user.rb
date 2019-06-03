d89bb08f807d72c43a8faaa10a5e76c44ddf1804
alexmpalermo <alexmpalermo@gmail.com>
2019-06-03 14:16:59 -0400

    Get rid of things I don't need

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   validates :name, :email, :password, presence: true
   validates :email, uniqueness: true
 
-  @@greetings = []
-
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_initialize do |user|
       user.name = auth.info.name
