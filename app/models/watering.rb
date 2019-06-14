0f47c34e0cc368614deb894caace784f45a73648
alexmpalermo <alexmpalermo@gmail.com>
2019-06-14 14:27:18 -0400

    Edit water method

--- app/models/watering.rb
+++ app/models/watering.rb
@@ -18,30 +18,26 @@ class Watering < ApplicationRecord
       @container = dispenser.containers.last
       @last = @container.waterings.last
       @plants.each do |plant|
-        @watering = Watering.create(:plant_id => plant.id, :date => Date.current, :container_id => @container.id, :end_vacation => @last.end_vacation, :start_vacation => @last.start_vacation, :vacation_days => v_days_calc(@last), :leftover => 0)
+        @days_array = (@last.end_vacation.to_date - Date.current).to_s.split("/")
+        @watering = Watering.create(:plant_id => plant.id, :date => Date.current, :container_id => @container.id, :end_vacation => @last.end_vacation, :start_vacation => @last.start_vacation, :vacation_days => @days_array[0].to_i, :leftover => 0)
         @watering.update(:leftover => (@watering.starting_amount(@last, @container) - plant.water_quantity))
         plant.update(:last_day_watered => @watering.date, :next_water_day => @watering.next_calc(plant))
-        refill_then_water_check(dispenser, @watering, plant)
+        @watering.refill_then_water_check(dispenser, plant)
       end
     end
-  end
-
-  def v_days_calc(last)
-    @days_array = (last.end_vacation.to_date - Date.current).to_s.split("/")
-    @days_array[0].to_i
   end
 
   def next_calc(plant)
     self.date.to_date + plant.water_frequency
   end
 
-  def refill_then_water_check(dispenser, watering, plant)
-    if watering.leftover < 0
+  def refill_then_water_check(dispenser, plant)
+    if self.leftover < 0
       @c_new = Container.refill(dispenser)
-      @w_new = Watering.create(:plant_id => plant.id, :container_id => @c_new.id, :vacation_days => watering.vacation_days, :start_vacation => watering.start_vacation, :end_vacation => watering.end_vacation, :date => watering.date, :leftover => (@c_new.start_amount + watering.leftover))
+      @w_new = Watering.create(:plant_id => plant.id, :container_id => @c_new.id, :vacation_days => self.vacation_days, :start_vacation => self.start_vacation, :end_vacation => self.end_vacation, :date => self.date, :leftover => (@c_new.start_amount + self.leftover))
       plant.check_water(@w_new.vacation_days)
     else
-      plant.check_water(watering.vacation_days)
+      plant.check_water(self.vacation_days)
     end
   end
 
