1c5914639bd9931ad1d00aacae5e24237514b9a4
alexmpalermo <alexmpalermo@gmail.com>
2019-07-02 13:58:54 -0400

    Make home page url /

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/home' => 'users#home', :as => :home
+  get '/' => 'users#home', :as => :home 
   get '/signup' => 'users#signup', :as => :signup
   post '/signup' => 'users#create'
   get '/login' => 'sessions#login', :as => :login
   post '/login' => 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  
+
 
   get '/auth/google_oauth2/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/home')
