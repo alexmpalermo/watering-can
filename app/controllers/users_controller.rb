8d0f0e24010932a74b78312e9dc46e787f372165
alexmpalermo <alexmpalermo@gmail.com>
2019-07-02 13:49:09 -0400

    Fix user update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,13 @@ class UsersController < ApplicationController
   def update
     if @user = User.find_by_id(params[:id])
       redirect_unless_logged_current
-      dispenser_plants_vaca_check(@user)
+      if @user.dispensers.empty?
+        flash[:error] = "You must register at least one Watering Can."
+        return redirect_to user_path(@user)
+      elsif @user.dispensers.detect {|disp| disp.plants.empty?}
+        flash[:error] = "One or more of your Watering Cans do not have plants assigned to them yet. Please add plants or delete the unused dispenser by visiting My Watering Cans."
+        return redirect_to user_path(@user)
+      end
       @vaca = params[:watering][:vacation_days]
       @user.update_vacation_and_plants(@vaca)
       flash[:notice] = "Vacation time has been successfully updated."
